8fba13983906f0788dd959d0851f13d9c9cd69f3
Muhammad Asif <asifcreativ@gmail.com>
2018-08-05 09:42:57 -0700

    create setup for learn JavaScript

---
+++ learn-js/script.js
@@ -0,0 +1,150 @@
+// 1. JAVASCRIPT TYPES
+/*
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null
+6. Symbol
+7. Object
+*/
+
+// 2. JAVASCRIPT COMPARISONS
+/*
+1. !==
+2. ===
+3. >=
+4. <=
+5. >
+6. <
+*/
+
+// 3. JAVASCRIPT CONDITIONS
+/*
+1. if
+2. else
+3. else if
+4. ternary
+5. switch
+*/
+
+// 4. JAVASCRIPT LOGICAL OPERATORS
+/*
+1. &&
+2. ||
+3. !
+*/
+
+// 5. JAVASCRIPT VARIABLES
+/*
+1. var
+2. let
+3. const
+*/
+
+// 6. JAVASCRIPT LOOPS
+/*
+1. for, for of, for in
+2. while
+3. do
+4. forEach
+*/
+
+// 7. JAVASCRIPT DATA STRUCTURE
+/*
+1. Array
+2. Object
+*/
+
+// 8. JAVASCRIPT FUNCTIONS
+/*
+1. function name () {} function declarations
+2. let name = function () {}
+3. () => {}
+*/
+
+// JavaScript Selectors
+/*
+DOM Selectors
+--------------
+getElementsByTagName
+getElementsByClassName
+getElementById
+
+querySelector
+querySelectorAll
+
+getAttribute
+setAttribute
+
+##Changing Styles
+style.{ property } //ok
+
+className //best
+classList //best
+
+classList.add
+classList.remove
+classList.toggle
+
+##Bonus
+innerHTML //DANGEROUS
+
+parentElement
+children
+*
+
+##It is important to CACHE selectors in variables
+*/
+
+// JAVASCRIPT DOM SELECTORS
+/*
+1. getElementByTagName
+2. getElementByClassName
+3. getElementById
+
+4. querySelector
+5. querySelectorAll
+
+1. getAttribute
+2. setAttribute
+
+1. style.{css property} = ''
+
+1. className
+2. classList
+    2.1 classList.add
+    2.2 classList.remove
+    2.3 classList.toggle
+
+innerHTML
+
+1. parentElement
+2. children
+*/
+
+// JAVASCRIPT DOM EVENTS
+/*
+document.createElement('{html element}')
+document.createTextNode('')
+appendChild()
+*/
+
+// =================
+// ES 5 & ES6
+// Destructuring
+// Template string
+// Arrow functions
+// 1. Closures
+// 2. Currying
+// 3. Compose
+
+// ================
+
+// Avoiding side effects and functional purity
+// Advanced Array
+/*
+1. map ... forEach
+2. filter
+3. reduce
+*/
